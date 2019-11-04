ef02ebe9045a0a7bd40cdcf40c147225bd6765a2
Zachary Zimmerman <zzim@microsoft.com>
2019-11-04 14:14:35 -0800

    play pause resume

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+var audioCtx;
+var oscillator;
+
+function setup() {
+    console.log("Setup audio context and oscillator...");
+    audioCtx = new AudioContext();
+    oscillator = audioCtx.createOscillator();
+    oscillator.type = 'square';
+    oscillator.connect(audioCtx.destination);
+}
+
+function start() {
+    console.log("Starting oscillator...");
+    oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+    oscillator.start();
+}
+
+function togglePlayback(button) {
+    if (audioCtx.state === 'running') {
+        audioCtx.suspend().then(() => {
+            button.textContent = 'Resume';
+        });
+        } else if (audioCtx.state === 'suspended') {
+        audioCtx.resume().then(() => {
+            button.textContent = 'Pause';
+        });  
+    }
+}
+
 window.onload = function() {
+    document.getElementById("setupBtn").onclick = () => {
+        setup();
+    }
     document.getElementById("startBtn").onclick = () => {
-        console.log("Starting...");
-        const audioCtx = new AudioContext();
-
-        // create Oscillator node
-        var oscillator = audioCtx.createOscillator();
-
-        oscillator.type = 'square';
-        oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
-        oscillator.connect(audioCtx.destination);
-        oscillator.start();
+        start();
+    }
+    var pauseBtn = document.getElementById("pauseBtn");
+    pauseBtn.onclick = () => {
+        togglePlayback(pauseBtn);
     }
 }
