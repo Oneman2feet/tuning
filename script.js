b734920f7beb3932a609ec727819f83b5decd8d6
Zachary Zimmerman <zzim@microsoft.com>
2019-11-04 13:28:57 -0800

    play a tone

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+window.onload = function() {
+    document.getElementById("startBtn").onclick = () => {
+        console.log("Starting...");
+        const audioCtx = new AudioContext();
+
+        // create Oscillator node
+        var oscillator = audioCtx.createOscillator();
+
+        oscillator.type = 'square';
+        oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+        oscillator.connect(audioCtx.destination);
+        oscillator.start();
+    }
+}
