e205996132fa118aff678d2e0fce495dd1c981dd
Zachary Zimmerman <Oneman2feet@gmail.com>
2021-02-01 17:38:05 -0500

    dev and prod

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Quickstart
 
 - `npm install`
+- `npm start`
+
+# Build
+
+- `npm install`
 - `npm run build`
 - open `dist/index.html` in your browser
--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/controls.js',
-  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Tuning',
