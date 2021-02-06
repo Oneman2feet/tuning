3f60fc82e9b515a1887ce220820979d8a9b65060
Zachary Zimmerman <Oneman2feet@gmail.com>
2021-02-06 00:18:29 -0500

    sort the tuning list in order of flat to sharp

--- src/metrics.js
+++ src/metrics.js
@@ -41,7 +41,7 @@ function updateTuningList(keyboard) {
     });
 
     // parse tuningList and update UI
-    var list = Object.keys(tuningList).sort().map((pitchClass) => "<tr><td>" + pitchClass + "</td><td>" + Array.from(tuningList[pitchClass]).map((cents) => "<span style='color:" + colorFromCents(cents) + ";'>" + cents + "</span>").join(", ") + "</td></tr>").join("");
+    var list = Object.keys(tuningList).sort().map((pitchClass) => "<tr><td>" + pitchClass + "</td><td>" + Array.from(tuningList[pitchClass]).sort((a, b) => parseInt(a) - parseInt(b)).map((cents) => "<span style='color:" + colorFromCents(cents) + ";'>" + cents + "</span>").join(", ") + "</td></tr>").join("");
     document.getElementById("tuningList").innerHTML = list;
 }
 
